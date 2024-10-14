eae0c644e093557bba7a6f389319a8aaa1c263b0
Eduardo Jaramillo <eduardo.jaramillo@metrodequito.gob.ec>
2024-10-14 17:01:33 -0500

    added route to consult by id and dates.

--- src/models/BillsModel.py
+++ src/models/BillsModel.py
@@ -10,14 +10,42 @@ class BillModel():
             bills=[]
             
             with connection.cursor() as cursor:
-                cursor.execute("SELECT id,concession_code,chs_data_dat,account_fiscal_id,ruta_xml_enviado,ruta_xml_recibido,ruta_xml_comprobante_recibido,ruta_pdf_generado FROM public.csv_data LIMIT 10;")
+                cursor.execute("SELECT id,chs_data_dat,account_fiscal_id,ruta_xml_comprobante_recibido,ruta_pdf_generado FROM public.csv_data LIMIT 10;")
                 resultset=cursor.fetchall()
-                #print("result",resultset)
                 for row in resultset:
-                    bill=Bill(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7])
+                    bill=Bill(row[0],row[1],row[2],row[3],row[4])
                     bills.append(bill.to_JSON())
             connection.close
             return bills
                     
         except Exception as ex:
             raise Exception(ex)
+        
+    @classmethod
+    def get_bill_by_id(self, identificacion=None, fecha_inicio=None, fecha_fin=None):
+        try:
+            connection=get_connection()
+            bills=[]
+            
+            with connection.cursor() as cursor:
+                query = """
+                    SELECT id, chs_data_dat, account_fiscal_id, ruta_xml_comprobante_recibido, ruta_pdf_generado 
+                    FROM public.csv_data
+                    WHERE (%s IS NULL OR account_fiscal_id = %s)
+                    AND (%s IS NULL OR chs_data_dat >= %s)
+                    AND (%s IS NULL OR chs_data_dat <= %s)
+                    LIMIT 10;
+                """
+                
+                cursor.execute(query, (identificacion, identificacion, fecha_inicio, fecha_inicio, fecha_fin, fecha_fin))
+                
+                resulset=cursor.fetchall()
+                for row in resulset:
+                    bill=Bill(row[0],row[1],row[2],row[3],row[4])
+                    bills.append(bill.to_JSON())
+            connection.close
+            return bills
+                    
+        except Exception as ex:
+            raise Exception(ex)
+            
--- src/routes/Bills.py
+++ src/routes/Bills.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 # Models
 from models.BillsModel import BillModel
@@ -9,8 +9,19 @@ main = Blueprint('bills_blueprint', __name__)
 @main.route('/')
 def get_bills():
     try:
-        bills = BillModel.get_bill()
-        print("retorno", bills)
+         # Imprimir todos los parámetros de consulta recibidos
+        print("Request args:", request.args)
+        
+        # Capturar parámetros de la URL
+        identificacion = request.args.get('identificacion')
+        fecha_inicio = request.args.get('fecha_inicio')
+        fecha_fin = request.args.get('fecha_fin')
+        
+        # Imprimir para depurar
+        print(f"identificacion: {identificacion}")
+        print(f"fecha_inicio: {fecha_inicio}")
+        print(f"fecha_fin: {fecha_fin}")
+        bills = BillModel.get_bill_by_id(identificacion= identificacion, fecha_inicio= fecha_inicio, fecha_fin=fecha_fin)
         return jsonify(bills)
     except Exception as ex:
         return jsonify({'message': str(ex)}), 500
