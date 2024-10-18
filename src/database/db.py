5c7de1692832a66ab46b49c6f74fe76f0eb0c3a6
stalincrisanto <stalincrisanto@hotmail.com>
2024-10-18 16:52:20 -0500

    feat: firts commit

--- src/database/db.py
+++ src/database/db.py
@@ -2,14 +2,25 @@ import psycopg2
 from psycopg2 import DatabaseError
 from decouple import config
 
+
 def get_connection():
     try:
-        return psycopg2.connect(
-            host=config('PGSQL_HOST'),
-            user=config('PGSQL_USER'),
-            password=config('PGSQL_PASSWORD'),
-            database=config('PGSQL_DATABASE'),
-            port= 5432            
+        print(f"Host: {config('PGSQL_HOST')}")
+        print(f"User: {config('PGSQL_USER')}")
+        print(f"Database: {config('PGSQL_DATABASE')}")
+        connection = psycopg2.connect(
+            host="127.0.0.1",
+            user="postgres",
+            password="301218",
+            database="postgres",
+            port=5432,
         )
+        print(connection)
+        connection.set_client_encoding("UTF8")  # O 'LATIN1' si es necesario
+        return connection
     except DatabaseError as ex:
-        raise ex
+        print(f"DatabaseError: {ex}")
+        raise
+    except Exception as ex:
+        print(f"Error inesperado: {ex}")
+        raise
--- src/models/BillsModel.py
+++ src/models/BillsModel.py
@@ -1,57 +1,70 @@
 from database.db import get_connection
 from .entities.Bill import Bill
 
-class BillModel():
-    
+
+class BillModel:
     @classmethod
     def get_bill_by_id(self, identificacion=None):
         try:
-            connection=get_connection()
-            bills=[]
-            
+            connection = get_connection()
+            bills = []
+
             with connection.cursor() as cursor:
-                query = ("""SELECT id,chs_data_dat,account_fiscal_id,
+                query = """SELECT id,concession_code,account_fiscal_id,
                                ruta_xml_comprobante_recibido,ruta_pdf_generado,
-                               account_razon FROM public.csv_data  
-                               WHERE  account_fiscal_id = %s LIMIT 10;""")
+                               account_razon FROM public.csv_srimvmn
+                               WHERE  account_fiscal_id = %s LIMIT 10;"""
                 cursor.execute(query, identificacion)
-                
-                resultset=cursor.fetchall()
+
+                resultset = cursor.fetchall()
+                print("resultset", resultset)
                 print(resultset)
                 for row in resultset:
-                    bill=Bill(row[0],row[1],row[2],row[3],row[4],row[5])
+                    bill = Bill(row[0], row[1], row[2], row[3], row[4], row[5])
                     bills.append(bill.to_JSON())
             connection.close
             return bills
-                    
+
         except Exception as ex:
             raise Exception(ex)
-        
+
     @classmethod
-    def get_bill_by_id_date(self, identificacion=None, fecha_inicio=None, fecha_fin=None):
+    def get_bill_by_id_date(
+        self, identificacion=None, fecha_inicio=None, fecha_fin=None
+    ):
         try:
-            connection=get_connection()
-            bills=[]
-            
+            connection = get_connection()
+            bills = []
+
             with connection.cursor() as cursor:
                 query = """
-                    SELECT id, chs_data_dat, account_fiscal_id, ruta_xml_comprobante_recibido, ruta_pdf_generado,account_razon
-                    FROM public.csv_data
+                    SELECT *
+                    FROM public.csv_srimvmn
                     WHERE (%s IS NULL OR account_fiscal_id = %s)
                     AND (%s IS NULL OR chs_data_dat >= %s)
                     AND (%s IS NULL OR chs_data_dat <= %s)
                     LIMIT 10;
                 """
-                
-                cursor.execute(query, (identificacion, identificacion, fecha_inicio, fecha_inicio, fecha_fin, fecha_fin))
-                
-                resulset=cursor.fetchall()
+
+                cursor.execute(
+                    query,
+                    (
+                        identificacion,
+                        identificacion,
+                        fecha_inicio,
+                        fecha_inicio,
+                        fecha_fin,
+                        fecha_fin,
+                    ),
+                )
+
+                resulset = cursor.fetchall()
                 for row in resulset:
-                    bill=Bill(row[0],row[1],row[2],row[3],row[4],row[5])
+                    bill = Bill(row[0], row[1], row[2], row[3], row[4], row[5])
                     bills.append(bill.to_JSON())
             connection.close
             return bills
-                    
+
         except Exception as ex:
+            print(ex)
             raise Exception(ex)
-            
